e2e6485c68b0ada5a7fc9f55466146320a4b9c53
BenPruden97 <benpruden34@yahoo.com>
2021-10-22 10:43:21 +0000

    Finished uploading and adding new testing videos and links to testing file

--- app.py
+++ app.py
@@ -520,4 +520,4 @@ def server_error(e):
 if __name__ == "__main__":
     app.run(host=os.environ.get("IP"),
             port=int(os.environ.get("PORT")),
-            debug=True)
+            debug=False)
